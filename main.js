3b71a49f2e56fcae7079f35ccab00e8942097dad
belltree-lang <belltree@belltree1102.com>
2026-03-07 16:47:31 +0900

    commit3

--- main.js
+++
@@ -1,9 +0,0 @@
-function initializeHomepage() {
-  // Future homepage enhancements will be initialized here.
-}
-
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', initializeHomepage);
-} else {
-  initializeHomepage();
-}
