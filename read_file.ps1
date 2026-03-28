2688982f58f61f60039676df22598e26638decae
belltree-lang <belltree@belltree1102.com>
2026-03-28 19:59:01 +0900

    commit177

--- read_file.ps1
+++
@@ -1,7 +0,0 @@
-$path = "c:\Users\sss_1\OneDrive\ドキュメント\GitHub\homepage - コピー\terms-of-service\index.html"
-try {
-    $c = [IO.File]::ReadAllText($path)
-    $c.Substring(0, [Math]::Min(10000, $c.Length))
-} catch {
-    $_.Exception.Message
-}
